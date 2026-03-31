c12e353f2b2a8f224807690a032ccdc6808e9f43
IgsonJr1 <igson.com@hotmail.com>
2026-03-31 14:24:19 -0400

    Add files via upload

---
+++ MediaNotesPro/PluginConfiguration.cs
@@ -0,0 +1,8 @@
+using MediaBrowser.Model.Plugins;
+
+namespace MediaNotesPro;
+
+public class PluginConfiguration : BasePluginConfiguration
+{
+    // Você pode adicionar opções aqui no futuro, como "Pasta de salvamento personalizada"
+}
