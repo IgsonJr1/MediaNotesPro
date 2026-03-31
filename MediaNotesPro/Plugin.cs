87a84ab5b4a5d8c2266091f69a9e2751bed83611
IgsonJr1 <igson.com@hotmail.com>
2026-03-31 14:24:01 -0400

    Create Plugin.cs

---
+++ MediaNotesPro/Plugin.cs
@@ -0,0 +1,40 @@
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using System.Collections.Generic;
+
+namespace MediaNotesPro;
+
+public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+{
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
+    {
+        Instance = this;
+    }
+
+    public override string Name => "Media Notes Pro";
+
+    public override Guid Id => Guid.Parse("a5e2f1b4-8c9a-4d2e-9b3c-1a2b3c4d5e6f");
+
+    public override string Description => "Crie anotações vinculadas ao tempo da mídia.";
+
+    public static Plugin? Instance { get; private set; }
+
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return new[]
+        {
+            new PluginPageInfo
+            {
+                Name = "medianotes",
+                EmbeddedResourcePath = "MediaNotesPro.Pages.notespage.html",
+                EnableInMainMenu = true,
+                MenuSection = "server",
+                MenuIcon = "edit_note",
+                DisplayName = "Media Notes Pro"
+            }
+        };
+    }
+}
